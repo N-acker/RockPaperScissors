a7bef9e7840ae1b599525e98edf0bc2001fd89e7
Harshk10-star <92046623+Harshk10-star@users.noreply.github.com>
2021-11-18 19:20:39 -0500

    Update rpsProject.js
    gg

--- rpsProject.js
+++ rpsProject.js
@@ -13,6 +13,6 @@ function computerPlay(){
     //0 representing rock
     //1 representing paper
     //2 repersenting siscors
-    var computerMove=Math.round(Math.random()*3)
+    var computerMove=Math.round(Math.random()*2)
 
 }
