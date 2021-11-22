5fcce9879aff38ba58173318f4d42a21f7c6e06c
Harshk10-star <92046623+Harshk10-star@users.noreply.github.com>
2021-11-22 18:25:34 -0500

    Update rpsProject.js

--- rpsProject.js
+++ rpsProject.js
@@ -7,13 +7,16 @@ window.onload=function()
 }
 function mainscreen(){
     var flexDiv=document.createElement("div");
+    $(flexDiv).attr("id","flex");
     var newPic=docment.createElement("img");
+    $(newPic).attr("id","picture");
     $(newPic).attr("src","images/homescreen_image/rps_homescreen.jpg")
     $(flexDiv).append(newPic);
     var discrition=docment.createElement("p");
     $(discrition).html("Rock paper scissors is a well known game which is quick and easy to play. To start our virtual rendition of the game, either choose single player mode or multiplayer mode. From here, you will be prompted to make a choice of rock, paper or scissors. Make your choice, and have fun!")
     var dDiscript=docmuent.createElement("div");
-    $(dDiscript).append(discrition)
+    $(dDiscript).attr("id","description");
+    $(dDiscript).append(discrition);
     $(flexDiv).append(dDiscript);
     $("#mainscreen").append(flexDiv);
   
