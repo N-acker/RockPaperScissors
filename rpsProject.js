05992aeeb772031a6985802f7f0fdd2cff17ee64
s-muntaha-b <saiedambegum@gmail.com>
2021-11-29 22:17:36 -0500

    .

--- rpsProject.js
+++ rpsProject.js
@@ -48,13 +48,12 @@ function mainscreen(){
 
 
 function singlePlayer(){
-
+    
     var playerCounter = document.getElementById("playerCounter");
     var compCounter = document.getElementById("compCounter");
 
     var playerCounter = 0;
     var compCounter = 0;
-    
     var choice=0;
     var singleDev = document.createElement("div");
     $("#mainscreen").empty();
