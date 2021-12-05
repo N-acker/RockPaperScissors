9f69a36b1f09384bc22037432410ce89f9d16839
Harshk10-star <92046623+Harshk10-star@users.noreply.github.com>
2021-12-04 19:23:50 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,26 @@
 
+
 var express = require('express');
 var app = express();
-var idCounter = 0;
 
 app.post('/post', (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
+    console.log("New express client");
+    console.log("Received: ");
+    console.log(JSON.parse(req.query['data']));
     var z = JSON.parse(req.query['data']);
 
-    
-    if (z["action"] == "evaulate"){
+    // check if the request action is generateCode
+    if (z["action"] == "generateCode"){
         var computerChoice=["rock","paper","scissors"];
-
- 
         var comPick=computerChoice[Math.round(Math.random()*2)];
+      
         var jsontext = JSON.stringify({
-            'action': 'computerGen'
+            'action': 'generateGen',
             'comPick': comPick,
         });
-       res.send(jsontext);
-        
+        res.send(jsontext);
+    }
+
 }).listen(3000);
 console.log("Server is running!");
-
